ed1a3839d0bc8f88a27616dc8c1781618208e300
Ivana Soric Strize <ivana.soric2@gmail.com>
2025-03-27 12:09:41 +0100

    CRUD functionality added.

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -25,5 +25,61 @@ namespace SimpleBookWebApi.Controllers
                 .ToListAsync());
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBookById(int id)
+        {
+            var book = await _db.Books.FindAsync(id);
+
+            if (book is null)
+                return NotFound();
+            
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> AddNewBook(Book newBook)
+        {
+            if (newBook is null)
+                return BadRequest();
+            
+            _db.Books.Add(newBook);
+            await _db.SaveChangesAsync();
+
+            return Created();
+            //return CreatedAtAction(nameof(GetBookById), new { id = newBook.Id }, newBook);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBook(int id, Book updatedBook)
+        {
+            var book = await _db.Books.FindAsync(id);
+
+            if (book is null)
+                return NotFound();
+            
+            book.Title = updatedBook.Title;
+            book.Author = updatedBook.Author;
+            book.Genres = updatedBook.Genres;
+            book.BookDetails = updatedBook.BookDetails;
+
+            await _db.SaveChangesAsync();
+            
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            var book = await _db.Books.FindAsync(id);
+
+            if (book is null)
+                return NotFound();
+
+            _db.Books.Remove(book);
+            await _db.SaveChangesAsync();
+            
+            return NoContent();
+        }
+
     }
 }
